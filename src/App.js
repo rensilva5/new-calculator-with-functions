600ec2c91f39f5fd7b290284fdd0e01555577703
rensilva5 <renzoasilva@gmail.com>
2022-11-14 15:06:45 -0500

    adding markups

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 function App() {
+
+  const createDigits = () => {
+    const digits = [];
+
+    for(let i = 1; i < 10; i++) {
+      digits.push(
+        <button key={i}>{i}</button>
+      )
+    }
+    return digits;
+  }
+
   return (
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span>(0)</span>0
+          <span>(0)</span> 0
         </div>
         <div className="operators">
           <button>/</button>
@@ -16,6 +28,7 @@ function App() {
         </div>
 
         <div className="digits">
+          { createDigits() }
           <button>0</button>
           <button>.</button>
           <button>=</button>
